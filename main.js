96e38b7393f1bc708db4270de12a8f2fdaa60178
Anderson Stiven Estrada Velasquez <anderson.velasquez@co.getcom.internal>
2022-11-02 14:22:18 -0500

    comments

---
+++ main.js
@@ -0,0 +1,9 @@
+const iconMenu = document.querySelector(".bx-menu");
+const menu = document.querySelector(".menu");
+
+
+
+iconMenu.addEventListener("click", function () {
+        (menu.classList.toggle("menu-show"));
+   
+});
